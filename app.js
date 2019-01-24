6b71db634f90deb930e38a05b6e838f762807029
borodkoleg <borodkoleg@gmail.com>
2019-01-24 19:40:43 +0200

    swagger+

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use('/api', require('./api/users'));
 app.use('/api', require('./api/words'));
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port);
