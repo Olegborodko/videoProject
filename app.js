7d8372b3b169e153a78281eb2a108b86c08087d8
borodkoleg <borodkoleg@gmail.com>
2019-01-26 20:15:00 +0200

    https+

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const swaggerDocument = require('./openapi.json');
 const app = express();
 const port = process.env.PORT || 8080;
 
+var fs = require('fs');
+var https = require('https');
+
 app.use('/api', require('./api/users'));
 app.use('/api', require('./api/words'));
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(port);
+https.createServer({
+    key: fs.readFileSync('./sslcert/key.pem'),
+    cert: fs.readFileSync('./sslcert/cert.pem')
+}, app)
+.listen(port);
